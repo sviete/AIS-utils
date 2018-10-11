dc47c898f25f5b26dfa021b8c0b732b799b95f40
Andrzej <araczkowski@gmail.com>
2018-10-11 13:17:21 +0200

    new installation file

--- caddy/get_caddy.sh
+++ caddy/get_caddy.sh
@@ -9,7 +9,9 @@
 # Download and extract #
 ########################
 
+echo "-----------------------------"
 echo "Downloading Caddy for AIS dom"
+echo "-----------------------------"
 
 # Use $PREFIX for compatibility with AIS dom on Android
 dl="$PREFIX/tmp/caddy.tar.gz"
@@ -17,18 +19,40 @@ rm -rf -- "$dl"
 
 curl -o "$dl" -L https://raw.githubusercontent.com/sviete/AIS-utils/master/caddy/caddy_v0.11.0_linux_arm7.tar.gz
 
-
+echo "-----------------------------"
 echo "Extracting..."
-tar xf "$dl"
+echo "-----------------------------"
+
+tar xf "$dl" -C "$PREFIX/tmp/"
 chmod +x "$PREFIX/tmp/caddy"
+chmod 777 "$PREFIX/tmp/Caddyfile"
 
-
+echo "-----------------------------"
 echo "Putting caddy in bin path"
+echo "-----------------------------"
+
 mv "$PREFIX/tmp/caddy" "$PREFIX/bin/caddy"
+mv "$PREFIX/tmp/Caddyfile" "$PREFIX/bin/Caddyfile"
 rm -rf "$dl"
 
-# check installation
-caddy -version
 
+echo "-----------------------------"
+echo "pm2 part"
+echo "-----------------------------"
+
+pm2 start caddy --name http -- -c "$PREFIX/bin/Caddyfile"
+pm2 save
+
+
+echo "-----------------------------"
+echo "check installation"
+caddy -version
+echo "-----------------------------"
+
+echo "-----------------------------"
 echo "Successfully installed"
+echo "-----------------------------"
+
+
+
 return 0
