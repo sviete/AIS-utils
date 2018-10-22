ba7ab02fb7771c7632055a35c99ac03204dc3d72
Andrzej <araczkowski@gmail.com>
2018-10-22 15:50:03 +0200

    reboot after install

--- upgrades/0.80.sh
+++ upgrades/0.80.sh
@@ -73,26 +73,12 @@ echo "Downloading GoogleApk for AIS dom"
 echo "-----------------------------"
 
 # Use $PREFIX for compatibility with AIS dom on Android
-dl="$PREFIX/tmp/Googlequicksearchbox.apk"
-rm -rf -- "$dl"
+rm -rf -- "/data/data/pl.sviete.dom/files/usr/tmp/Googlequicksearchbox.apk"
 
-curl -o "$dl" -L https://raw.githubusercontent.com/sviete/AIS-utils/master/android/apks/Googlequicksearchbox.apk
+curl -o "/data/data/pl.sviete.dom/files/usr/tmp/Googlequicksearchbox.apk" -L https://raw.githubusercontent.com/sviete/AIS-utils/master/android/apks/Googlequicksearchbox.apk
 
 echo "-----------------------------"
 echo "Installing the Googlequicksearchbox..."
 echo "-----------------------------"
 
-echo "remount system rw"
-su -c "mount -o rw,remount,rw /system"
-
-echo "mv app from $dl to /system/priv-app/Googlequicksearchbox.apk"
-su -c "mv $dl /system/priv-app/Googlequicksearchbox.apk"
-
-echo "chmod 644 /system/priv-app/Googlequicksearchbox.apk"
-su -c "chmod 644 /system/priv-app/Googlequicksearchbox.apk"
-
-echo "remount system ro"
-su -c "mount -o ro,remount,ro /system"
-
-echo "reboot"
-su -c "reboot"
+su -c "mount -o rw,remount,rw /system && mv /data/data/pl.sviete.dom/files/usr/tmp/Googlequicksearchbox.apk /system/priv-app/Googlequicksearchbox.apk && chmod 644 /system/priv-app/Googlequicksearchbox.apk && mount -o ro,remount,ro /system && reboot"
