0c7bc748e41ab2378356049f5fce9bc5327250eb
Andrzej <araczkowski@gmail.com>
2018-10-24 10:06:46 +0200

    start and stop time

--- upgrades/0.80.sh
+++ upgrades/0.80.sh
@@ -8,6 +8,14 @@
 # 1. Download and extract Caddy HTTP server    #
 ################################################
 
+echo $(date '+%Y %b %d %H:%M') START
+echo "-----------------------------"
+echo "AIS dom upgrade to version 0.80.x"
+echo "-----------------------------"
+
+echo "-----------------------------"
+echo "apt install libxslt-dev"
+apt install -y libxslt-dev
 
 echo "-----------------------------"
 echo "Downloading Caddy for AIS dom"
@@ -39,9 +47,12 @@ rm -rf "$dl"
 echo "-----------------------------"
 echo "pm2 part"
 echo "-----------------------------"
-pm2 delete http
-pm2 start caddy --name http -- -conf "$PREFIX/bin/Caddyfile"
-pm2 save
+echo "pm2 delete http"
+/data/data/pl.sviete.dom/files/usr/bin/pm2 delete http
+echo "pm2 start caddy"
+/data/data/pl.sviete.dom/files/usr/bin/pm2 start caddy --name http -- -conf "$PREFIX/bin/Caddyfile"
+echo "pm2 start caddy"
+/data/data/pl.sviete.dom/files/usr/bin/pm2 save
 
 
 echo "-----------------------------"
@@ -60,3 +71,4 @@ echo "-----------------------------"
 echo "HDMI 4K to build.prop start"
 su -c "mount -o rw,remount,rw /system && sed -i '/ro.platform.support.over.4k30/d' '/system/build.prop' &&  echo 'ro.platform.support.over.4k30=false' >> /system/build.prop && mount -o ro,remount,ro /system && echo 'done'"
 echo "HDMI 4K to build.prop done"
+echo $(date '+%Y %b %d %H:%M') STOP 
