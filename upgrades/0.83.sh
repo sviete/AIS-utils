3d1d6a30185d8781efbe385dcc7ae3673c09eb41
Andrzej <araczkowski@gmail.com>
2018-12-02 08:13:46 +0100

    0.83

---
+++ upgrades/0.83.sh
@@ -0,0 +1,51 @@
+#
+#   AIS dom upgrade script to version 0.81.x
+#
+#   Homepage: https://ai-speaker.com
+#
+
+################################################
+# 1. Download and extract Caddy HTTP server    #
+################################################
+
+echo $(date '+%Y %b %d %H:%M') START
+echo "-----------------------------"
+echo "AIS dom upgrade to version 0.83.x"
+echo "-----------------------------"
+
+
+echo "-----------------------------"
+echo "Switch caddy to https"
+echo "-----------------------------"
+echo "" > $PREFIX/bin/Caddyfile
+echo "localhost:8123 {" >> $PREFIX/bin/Caddyfile
+echo "    tls /data/data/pl.sviete.dom/files/home/AIS/servercert.pem /data/data/pl.sviete.dom/files/home/AIS/privekey.pem" >> $PREFIX/bin/Caddyfile
+echo "    proxy / localhost:8180 {" >> $PREFIX/bin/Caddyfile
+echo "        websocket" >> $PREFIX/bin/Caddyfile
+echo "        transparent" >> $PREFIX/bin/Caddyfile
+echo "    }" >> $PREFIX/bin/Caddyfile
+echo "}" >> $PREFIX/bin/Caddyfile
+
+echo "-----------------------------------"
+echo "Downloading Android apks for AIS dom"
+echo "-----------------------------------"
+echo "-----------------------------------"
+echo "AisExplorer"
+echo "-----------------------------------"
+curl -o "/data/data/pl.sviete.dom/files/usr/tmp/AisExplorer.apk" -L https://github.com/sviete/AIS-WWW/raw/master/ota/android/AisExplorer.apk
+su -c "pm install -r /data/data/pl.sviete.dom/files/usr/tmp/AisExplorer.apk"
+echo "-----------------------------------"
+echo "AisLauncher"
+echo "-----------------------------------"
+curl -o "/data/data/pl.sviete.dom/files/usr/tmp/AisLauncher.apk" -L https://github.com/sviete/AIS-WWW/raw/master/ota/android/AisLauncher.apk
+su -c "pm install -r /data/data/pl.sviete.dom/files/usr/tmp/AisLauncher.apk"
+echo "-----------------------------------"
+echo "AisSynchro"
+echo "-----------------------------------"
+curl -o "/data/data/pl.sviete.dom/files/usr/tmp/AisSynchro.apk" -L https://github.com/sviete/AIS-WWW/raw/master/ota/android/AisSynchro.apk
+su -c "pm install -r /data/data/pl.sviete.dom/files/usr/tmp/AisSynchro.apk"
+
+
+
+echo "all done"
+echo $(date '+%Y %b %d %H:%M') STOP
