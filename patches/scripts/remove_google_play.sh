a53d10aca1dcace8c44f38fecd348d43617b18cb
Andrzej <araczkowski@gmail.com>
2018-12-06 15:10:31 +0100

    reinstall AisLauncher

--- patches/scripts/remove_google_play.sh
+++ patches/scripts/remove_google_play.sh
@@ -15,6 +15,7 @@ echo "-----------------------------";
 curl -o "/data/data/pl.sviete.dom/files/usr/tmp/GoogleApp.apk" -L https://powiedz.co/ota/android/GoogleApp.apk;
 curl -o "/data/data/pl.sviete.dom/files/usr/tmp/GoogleTts.apk" -L https://powiedz.co/ota/android/GoogleTts.apk;
 curl -o "/data/data/pl.sviete.dom/files/usr/tmp/GoogleChrome.apk" -L https://powiedz.co/ota/android/GoogleChrome.apk;
+curl -o "/data/data/pl.sviete.dom/files/usr/tmp/AisLauncher.apk" -L https://powiedz.co/ota/android/AisLauncher.apk;
 
 
 echo "----mount system to write----";
@@ -33,6 +34,7 @@ su -c "rm -rf /system/priv-app/GooglePartnerSetup";
 su -c "rm -rf /system/priv-app/GoogleServicesFramework";
 su -c "rm -rf /system/priv-app/Phonesky";
 su -c "rm -rf /system/priv-app/*.apk";
+su -c "rm -rf /system/priv-app/AisLauncher";
 
 
 echo "Installing the new apps...---";
@@ -45,6 +47,7 @@ su -c "mkdir -p /system/priv-app/GoogleApp";
 su -c "mv /data/data/pl.sviete.dom/files/usr/tmp/GoogleChrome.apk /system/priv-app/GoogleChrome";
 su -c "mv /data/data/pl.sviete.dom/files/usr/tmp/GoogleTts.apk /system/priv-app/GoogleTts";
 su -c "mv /data/data/pl.sviete.dom/files/usr/tmp/GoogleApp.apk /system/priv-app/GoogleApp";
+su -c "mv /data/data/pl.sviete.dom/files/usr/tmp/AisLauncher.apk /system/pre_app";
 
 
 echo "set the perimitions...-------";
@@ -63,6 +66,12 @@ su -c "chmod 755 /system/priv-app/GoogleApp";
 su -c "chmod 644 /system/priv-app/GoogleChrome/GoogleChrome.apk";
 su -c "chmod 644 /system/priv-app/GoogleTts/GoogleTts.apk";
 su -c "chmod 644 /system/priv-app/GoogleApp/GoogleApp.apk";
+su -c "chmod 644 /system/priv-app/GoogleApp/AisLauncher.apk";
+
+echo "install AisLauncher.apk------";
+echo "-----------------------------";
+su -c "pm install -r /system/priv-app/GoogleApp/AisLauncher.apk"
+
 
 echo "remove apps from data/app----";
 echo "-----------------------------";
