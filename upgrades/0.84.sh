faa423f8d362e61b112384518a145d1ff76da260
Andrzej <araczkowski@gmail.com>
2018-12-10 15:08:21 +0100

    AccessibilityService

--- upgrades/0.84.sh
+++ upgrades/0.84.sh
@@ -42,25 +42,24 @@ echo "-----------------------------------"
 echo "-----------------------------------"
 echo "AisExplorer"
 echo "-----------------------------------"
-curl -o "/data/data/pl.sviete.dom/files/usr/tmp/AisExplorer.apk" -L https://github.com/sviete/AIS-WWW/raw/master/ota/android/AisExplorer.apk
-su -c "pm install -r /data/data/pl.sviete.dom/files/usr/tmp/AisExplorer.apk"
+curl -o "/data/data/pl.sviete.dom/files/usr/tmp/AisExplorer.apk" -L https://github.com/sviete/AIS-WWW/raw/master/ota/android/AisExplorer.apk &&
+su -c "pm install -r /data/data/pl.sviete.dom/files/usr/tmp/AisSynchro.apk" &&
 echo "-----------------------------------"
 echo "AisLauncher"
 echo "-----------------------------------"
-curl -o "/data/data/pl.sviete.dom/files/usr/tmp/AisLauncher.apk" -L https://github.com/sviete/AIS-WWW/raw/master/ota/android/AisLauncher.apk
-su -c "pm install -r /data/data/pl.sviete.dom/files/usr/tmp/AisLauncher.apk"
+curl -o "/data/data/pl.sviete.dom/files/usr/tmp/AisLauncher.apk" -L https://github.com/sviete/AIS-WWW/raw/master/ota/android/AisLauncher.apk &&
+su -c "pm install -r /data/data/pl.sviete.dom/files/usr/tmp/AisExplorer.apk" &&
 echo "-----------------------------------"
 echo "AisSynchro"
 echo "-----------------------------------"
-curl -o "/data/data/pl.sviete.dom/files/usr/tmp/AisSynchro.apk" -L https://github.com/sviete/AIS-WWW/raw/master/ota/android/AisSynchro.apk
-su -c "pm install -r /data/data/pl.sviete.dom/files/usr/tmp/AisSynchro.apk"
+curl -o "/data/data/pl.sviete.dom/files/usr/tmp/AisSynchro.apk" -L https://github.com/sviete/AIS-WWW/raw/master/ota/android/AisSynchro.apk &&
+su -c "pm install -r /data/data/pl.sviete.dom/files/usr/tmp/AisLauncher.apk" &&
+
 
 echo "-----------------------------------"
 echo "New grants"
 echo "-----------------------------------"
-su -c "pm grant pl.sviete.dom android.permission.WRITE_SECURE_SETTINGS"
-su -c "pm grant pl.sviete.dom android.permission.CHANGE_CONFIGURATION"
-su -c "settings put secure enabled_accessibility_services %accessibility:pl.sviete.dom/pl.sviete.dom.DomAccessibilityService"
+su -c "settings put secure enabled_accessibility_services %accessibility:pl.sviete.dom/pl.sviete.dom.DomAccessibilityService" &&
 
 echo "all done"
 echo $(date '+%Y %b %d %H:%M') STOP
