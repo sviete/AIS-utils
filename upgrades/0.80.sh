76b9f60fb5401f0b015d74cbbd484da2a182c2f1
araczkowski <araczkowski@gmail.com>
2019-02-21 18:27:38 +0100

    0.87 final

--- upgrades/0.80.sh
+++ upgrades/0.80.sh
@@ -13,6 +13,8 @@ echo "-----------------------------"
 echo "AIS dom upgrade to version 0.80.x"
 echo "-----------------------------"
 
+curl --header "Content-Type: application/json" --max-time 2 --request POST --data '{"text":"Start aktualizacji do wersji 0.80. To potrwa 5 minut. Poczekaj."}' http://localhost:8122/text_to_speech &&
+
 echo "-----------------------------"
 echo "apt install libxslt-dev"
 apt install -y libxslt-dev
@@ -52,8 +54,6 @@ ssh localhost -o StrictHostKeyChecking=no -p 8022 -i /data/data/pl.sviete.dom/fi
 pm2 delete http
 pm2 start caddy --name http -- -conf "$PREFIX/bin/Caddyfile"
 pm2 save
-npm install -g dom-tunnel
-curl -o "/data/data/pl.sviete.dom/files/home/AIS/.storage/lovelace" -L  https://raw.githubusercontent.com/sviete/AIS-utils/master/patches/scripts/lovelace
 EOF
 echo "inception... go back"
 
@@ -75,14 +75,81 @@ echo "HDMI 4K to build.prop start"
 su -c "mount -o rw,remount,rw /system && sed -i '/ro.platform.support.over.4k30/d' '/system/build.prop' &&  echo 'ro.platform.support.over.4k30=false' >> /system/build.prop && mount -o ro,remount,ro /system && echo 'done'"
 echo "HDMI 4K to build.prop done"
 
-echo "Upgrade pip------------------";
+
+
+# ################################################
+#   AIS dom upgrade script to version 0.87.x
+#
+#   Homepage: https://ai-speaker.com
+#
+# curl https://raw.githubusercontent.com/sviete/AIS-utils/master/upgrades/0.87.sh | bash
+################################################
+echo "-----------------------------"
+echo "-----------------------------"
+echo "-----------------------------"
+echo $(date '+%Y %b %d %H:%M') START
+echo "-----------------------------"
+echo "AIS dom upgrade to version 0.87.x"
+echo "-----------------------------"
+echo "-----------------------------"
+curl --header "Content-Type: application/json" --max-time 2 --request POST --data '{"text":"Start aktualizacji do wersji 0.87.3. To potrwa 30 minut. Poczekaj."}' http://localhost:8122/text_to_speech &&
+echo "-----------------------------"
 echo "-----------------------------";
-pip install pip -U
+echo "Install make and git---------";
 echo "-----------------------------";
+apt -y install make git &&
+curl --header "Content-Type: application/json" --max-time 2 --request POST --data '{"text":"Aktualizacja, mamy 5%"}' http://localhost:8122/text_to_speech &&
+echo "-----------------------------";
+echo "Install pynacl and webssh----";
+echo "-----------------------------";
+cd ~
+git clone https://github.com/araczkowski/pynacl.git
+cd pynacl
+curl --header "Content-Type: application/json" --max-time 2 --request POST --data '{"text":"Kompilacja. Ten krok potrwa 15 minut. Poczekaj."}' http://localhost:8122/text_to_speech &&
+python setup.py install
+cd ~
+rm -rf pynacl
+curl --header "Content-Type: application/json" --max-time 2 --request POST --data '{"text":"Aktualizacja, mamy 55%"}' http://localhost:8122/text_to_speech &&
+git clone https://github.com/araczkowski/webssh.git
+cd webssh
+python setup.py install
+cd ~
+rm -rf webssh
+echo "We will generate ssh id_rsa private key file now..."
+cd ~/.ssh
+rm rsa-key
+rm rsa-key.pub
+ssh-keygen -m PEM -t rsa -f rsa-key -N ""
+cat ~/.ssh/rsa-key.pub >> ~/.ssh/authorized_keys
+sed -i -e '$a\' ~/.ssh/authorized_keys
+echo "RSA key generation done"
+
+
+echo "-----------------------------";
+echo "Install pm service ----------";
+echo "-----------------------------";
+echo "inception... go deeper..."
+ssh localhost -o StrictHostKeyChecking=no -p 8022 -i /data/data/pl.sviete.dom/files/home/AIS/www/id_rsa_ais << EOF
+pm2 delete webssh
+pm2 start wssh --name webssh --interpreter=python
+pm2 save
+npm install -g dom-tunnel
+EOF
+echo "inception... go back"
+curl --header "Content-Type: application/json" --max-time 2 --request POST --data '{"text":"Aktualizacja, mamy 65%"}' http://localhost:8122/text_to_speech &&
+
 echo "-----------------------------";
 echo "Downloading lovelace json----";
 echo "-----------------------------";
-curl -o "/data/data/pl.sviete.dom/files/home/AIS/.storage/lovelace" -L  https://raw.githubusercontent.com/sviete/AIS-utils/master/patches/scripts/lovelace;
+curl -o "/data/data/pl.sviete.dom/files/home/AIS/.storage/lovelace" -L  https://raw.githubusercontent.com/sviete/AIS-utils/master/patches/scripts/lovelace
+curl -o "/data/data/pl.sviete.dom/files/home/.bash_profile" -L  https://raw.githubusercontent.com/sviete/AIS-utils/master/patches/scripts/.bash_profile
+chmod +x /data/data/pl.sviete.dom/files/home/.bash_profile
+curl -o "/data/data/pl.sviete.dom/files/usr/etc/motd" -L  https://raw.githubusercontent.com/sviete/AIS-utils/master/patches/scripts/motd
+echo "-----------------------------";
+echo "Upgrade pip------------------";
+echo "-----------------------------";
+pip install pip -U;
 
-
+curl --header "Content-Type: application/json" --max-time 2 --request POST --data '{"text":"Aktualizacja Asystenta domowego, mamy 75%, poczekaj na restart."}' http://localhost:8122/text_to_speech &&
+echo "all done"
 echo $(date '+%Y %b %d %H:%M') STOP
--- upgrades/0.83.sh
+++ upgrades/0.83.sh
@@ -12,7 +12,7 @@ echo $(date '+%Y %b %d %H:%M') START
 echo "-----------------------------"
 echo "AIS dom upgrade to version 0.83.x"
 echo "-----------------------------"
-
+curl --header "Content-Type: application/json" --max-time 2 --request POST --data '{"text":"Start aktualizacji do wersji 0.83. To potrwa 5 minut. Poczekaj."}' http://localhost:8122/text_to_speech &&
 echo "-----------------------------"
 echo "Switch caddy to https"
 echo "-----------------------------"
@@ -32,19 +32,83 @@ ssh localhost -o StrictHostKeyChecking=no -p 8022 -i /data/data/pl.sviete.dom/fi
 pm2 restart http
 pm2 delete nred
 pm2 save
-npm install -g dom-tunnel
-curl -o "/data/data/pl.sviete.dom/files/home/AIS/.storage/lovelace" -L  https://raw.githubusercontent.com/sviete/AIS-utils/master/patches/scripts/lovelace
 EOF
 echo "inception... go back"
 
+
+#
+#   AIS dom upgrade script to version 0.87.x
+#
+#   Homepage: https://ai-speaker.com
+#
+# curl https://raw.githubusercontent.com/sviete/AIS-utils/master/upgrades/0.87.sh | bash
+################################################
+echo "-----------------------------"
+echo "-----------------------------"
+echo "-----------------------------"
+echo $(date '+%Y %b %d %H:%M') START
+echo "-----------------------------"
+echo "AIS dom upgrade to version 0.87.x"
+echo "-----------------------------"
+echo "-----------------------------"
+curl --header "Content-Type: application/json" --max-time 2 --request POST --data '{"text":"Start aktualizacji do wersji 0.87.3. To potrwa 30 minut. Poczekaj."}' http://localhost:8122/text_to_speech &&
+echo "-----------------------------"
 echo "-----------------------------";
-echo "Upgrade pip------------------";
+echo "Install make and git---------";
 echo "-----------------------------";
-pip install pip -U
+apt -y install make git &&
+curl --header "Content-Type: application/json" --max-time 2 --request POST --data '{"text":"Aktualizacja, mamy 5%"}' http://localhost:8122/text_to_speech &&
+echo "-----------------------------";
+echo "Install pynacl and webssh----";
+echo "-----------------------------";
+cd ~
+git clone https://github.com/araczkowski/pynacl.git
+cd pynacl
+curl --header "Content-Type: application/json" --max-time 2 --request POST --data '{"text":"Kompilacja. Ten krok potrwa 15 minut. Poczekaj."}' http://localhost:8122/text_to_speech &&
+python setup.py install
+cd ~
+rm -rf pynacl
+curl --header "Content-Type: application/json" --max-time 2 --request POST --data '{"text":"Aktualizacja, mamy 55%"}' http://localhost:8122/text_to_speech &&
+git clone https://github.com/araczkowski/webssh.git
+cd webssh
+python setup.py install
+cd ~
+rm -rf webssh
+echo "We will generate ssh id_rsa private key file now..."
+cd ~/.ssh
+rm rsa-key
+rm rsa-key.pub
+ssh-keygen -m PEM -t rsa -f rsa-key -N ""
+cat ~/.ssh/rsa-key.pub >> ~/.ssh/authorized_keys
+sed -i -e '$a\' ~/.ssh/authorized_keys
+echo "RSA key generation done"
+
+
+echo "-----------------------------";
+echo "Install pm service ----------";
+echo "-----------------------------";
+echo "inception... go deeper..."
+ssh localhost -o StrictHostKeyChecking=no -p 8022 -i /data/data/pl.sviete.dom/files/home/AIS/www/id_rsa_ais << EOF
+pm2 delete webssh
+pm2 start wssh --name webssh --interpreter=python
+pm2 save
+npm install -g dom-tunnel
+EOF
+echo "inception... go back"
+curl --header "Content-Type: application/json" --max-time 2 --request POST --data '{"text":"Aktualizacja, mamy 65%"}' http://localhost:8122/text_to_speech &&
+
 echo "-----------------------------";
 echo "Downloading lovelace json----";
 echo "-----------------------------";
-curl -o "/data/data/pl.sviete.dom/files/home/AIS/.storage/lovelace" -L  https://raw.githubusercontent.com/sviete/AIS-utils/master/patches/scripts/lovelace;
+curl -o "/data/data/pl.sviete.dom/files/home/AIS/.storage/lovelace" -L  https://raw.githubusercontent.com/sviete/AIS-utils/master/patches/scripts/lovelace
+curl -o "/data/data/pl.sviete.dom/files/home/.bash_profile" -L  https://raw.githubusercontent.com/sviete/AIS-utils/master/patches/scripts/.bash_profile
+chmod +x /data/data/pl.sviete.dom/files/home/.bash_profile
+curl -o "/data/data/pl.sviete.dom/files/usr/etc/motd" -L  https://raw.githubusercontent.com/sviete/AIS-utils/master/patches/scripts/motd
+echo "-----------------------------";
+echo "Upgrade pip------------------";
+echo "-----------------------------";
+pip install pip -U;
 
+curl --header "Content-Type: application/json" --max-time 2 --request POST --data '{"text":"Aktualizacja Asystenta domowego, mamy 75%, poczekaj na restart."}' http://localhost:8122/text_to_speech &&
 echo "all done"
 echo $(date '+%Y %b %d %H:%M') STOP
