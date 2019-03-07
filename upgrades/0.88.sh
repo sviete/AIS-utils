c321ebf56ed77062b3c1f53aa76a0bc9f5ee8c13
araczkowski <araczkowski@gmail.com>
2019-03-07 18:27:48 +0100

    Model name to build.prop

--- upgrades/0.88.sh
+++ upgrades/0.88.sh
@@ -22,5 +22,10 @@ curl -o "/sdcard/ais-dom-0.88.2.tar.gz" -L  https://raw.githubusercontent.com/sv
 pip install /sdcard/ais-dom-0.88.2.tar.gz -U;
 echo "-----------------------------";
 curl --header "Content-Type: application/json" --max-time 2 --request POST --data '{"text":"Aktualizacja wykonana, poczekaj na restart."}' http://localhost:8122/text_to_speech &&
+
+echo "Model name to build.prop start"
+su -c "mount -o rw,remount,rw /system && sed -i '/ro.product.model=p281/d' '/system/build.prop' &&  echo 'ro.product.model=AI-Speaker.com' >> /system/build.prop && mount -o ro,remount,ro /system && echo 'done'"
+echo "Model name to build.prop done"
+
 echo "all done"
 echo $(date '+%Y %b %d %H:%M') STOP
