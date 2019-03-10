524e576bd9e8509d8f4e3f624853b81700494153
araczkowski <araczkowski@gmail.com>
2019-03-10 13:34:54 +0100

    0.89

--- upgrades/0.89.sh
+++ upgrades/0.89.sh
@@ -3,14 +3,14 @@
 #
 #   Homepage: https://ai-speaker.com
 #
-# curl https://raw.githubusercontent.com/sviete/AIS-utils/master/upgrades/0.88.sh | bash
+# curl https://raw.githubusercontent.com/sviete/AIS-utils/master/upgrades/0.89.sh | bash
 ################################################
 echo "-----------------------------"
 echo "-----------------------------"
 echo "-----------------------------"
 echo $(date '+%Y %b %d %H:%M') START
 echo "-----------------------------"
-echo "AIS dom upgrade to version 0.87.x"
+echo "AIS dom upgrade to version 0.89.x"
 echo "-----------------------------"
 echo "-----------------------------"
 curl --header "Content-Type: application/json" --max-time 2 --request POST --data '{"text":"Start aktualizacji. To potrwa 10 minut. Poczekaj."}' http://localhost:8122/text_to_speech &&
@@ -18,8 +18,8 @@ curl --header "Content-Type: application/json" --max-time 2 --request POST --dat
 
 echo "Install ais-dom--------------";
 echo "-----------------------------";
-curl -o "/sdcard/ais-dom-0.88.2.tar.gz" -L  https://raw.githubusercontent.com/sviete/AIS-utils/master/patches/scripts/ais-dom-0.88.2.tar.gz;
-pip install /sdcard/ais-dom-0.88.2.tar.gz -U;
+curl -o "/sdcard/ais-dom-0.89.1.tar.gz" -L  https://raw.githubusercontent.com/sviete/AIS-utils/master/patches/scripts/ais-dom-0.89.1.tar.gz;
+pip install /sdcard/ais-dom-0.89.1.tar.gz -U;
 echo "-----------------------------";
 
 curl --header "Content-Type: application/json" --max-time 2 --request POST --data '{"text":"Instaluje Spotify."}' http://localhost:8122/text_to_speech &&
@@ -27,21 +27,21 @@ curl --header "Content-Type: application/json" --max-time 2 --request POST --dat
 echo "-----------------------------";
 echo "Downloading lovelace json----";
 echo "-----------------------------";
-curl -o "/data/data/pl.sviete.dom/files/home/AIS/.storage/lovelace" -L  https://raw.githubusercontent.com/sviete/AIS-utils/master/patches/scripts/lovelace
+curl -o "/data/data/pl.sviete.dom/files/home/AIS/.storage/lovelace" -L  https://raw.githubusercontent.com/sviete/AIS-utils/master/patches/scripts/lovelace &&
 echo "-----------------------------";
 echo "Downloading aps for AIS dom";
 echo "-----------------------------";
-curl -o "/sdcard/Spotify.apk" -L https://powiedz.co/ota/android/Spotify.apk;
+curl -o "/sdcard/Spotify.apk" -L https://powiedz.co/ota/android/Spotify.apk &&
 echo "-----------------------------";
 echo "Install Spotify.apk------";
 echo "-----------------------------";
-su -c "pm install -r /sdcard/Spotify.apk"
+su -c "pm install -r /sdcard/Spotify.apk" &&
 
 curl --header "Content-Type: application/json" --max-time 2 --request POST --data '{"text":"Aktualizacja wykonana, poczekaj na restart."}' http://localhost:8122/text_to_speech &&
 
 
 echo "Model name to build.prop start"
-su -c "mount -o rw,remount,rw /system && sed -i '/ro.product.model=p281/d' '/system/build.prop' &&  echo 'ro.product.model=AI-Speaker.com' >> /system/build.prop && mount -o ro,remount,ro /system && echo 'done'"
+su -c "mount -o rw,remount,rw /system && sed -i '/ro.product.model=p281/d' '/system/build.prop' &&  echo 'ro.product.model=AI-Speaker.com' >> /system/build.prop && mount -o ro,remount,ro /system && echo 'done'" &&
 echo "Model name to build.prop done"
 
 echo "all done"
