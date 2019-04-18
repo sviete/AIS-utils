93c83802e41cd1f7c3b5a6bfecc45d50b6c71291
araczkowski <araczkowski@gmail.com>
2019-04-18 16:15:59 +0200

    0.91.4 final

--- upgrades/0.91.sh
+++ upgrades/0.91.sh
@@ -20,19 +20,7 @@ echo "Downloading lovelace json----";
 echo "-----------------------------";
 curl -o "/data/data/pl.sviete.dom/files/home/AIS/.storage/lovelace" -L  https://raw.githubusercontent.com/sviete/AIS-utils/master/patches/scripts/lovelace &&
 echo "-----------------------------";
-echo "Downloading aps for AIS dom";
 echo "-----------------------------";
-curl -o "/sdcard/ais-dom-0.91.4.tar.gz" -L https://raw.githubusercontent.com/sviete/AIS-utils/master/patches/scripts/ais-dom-0.91.4.tar.gz &&
-
-curl --header "Content-Type: application/json" --max-time 2 --request POST --data '{"text":"Instaluje"}' http://localhost:8122/text_to_speech &&
-
-pip install /sdcard/ais-dom-0.91.4.tar.gz -U &&
-
-curl --header "Content-Type: application/json" --max-time 2 --request POST --data '{"text":"Zainstalowane, poczekaj na koniec aktualizacji i restart."}' http://localhost:8122/text_to_speech &&
-
-curl -o "/sdcard/AisPanelApp-test.apk" -L https://powiedz.co/ota/android/AisPanelApp-test.apk &&
-su -c "pm install -r /sdcard/AisPanelApp-test.apk" &&
-
 
 echo "all done"
 echo $(date '+%Y %b %d %H:%M') STOP
