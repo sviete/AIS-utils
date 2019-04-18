3ebc5a3be83df1253128b1eaf3d2b4e2abfc1921
araczkowski <araczkowski@gmail.com>
2019-04-18 12:30:58 +0200

    0.91.4

--- upgrades/0.91.sh
+++ upgrades/0.91.sh
@@ -22,11 +22,11 @@ curl -o "/data/data/pl.sviete.dom/files/home/AIS/.storage/lovelace" -L  https://
 echo "-----------------------------";
 echo "Downloading aps for AIS dom";
 echo "-----------------------------";
-curl -o "/sdcard/ais-dom-0.91.3.tar.gz" -L https://raw.githubusercontent.com/sviete/AIS-utils/master/patches/scripts/ais-dom-0.91.3.tar.gz &&
+curl -o "/sdcard/ais-dom-0.91.4.tar.gz" -L https://raw.githubusercontent.com/sviete/AIS-utils/master/patches/scripts/ais-dom-0.91.4.tar.gz &&
 
 curl --header "Content-Type: application/json" --max-time 2 --request POST --data '{"text":"Instaluje"}' http://localhost:8122/text_to_speech &&
 
-pip install /sdcard/ais-dom-0.91.3.tar.gz -U &&
+pip install /sdcard/ais-dom-0.91.4.tar.gz -U &&
 
 curl --header "Content-Type: application/json" --max-time 2 --request POST --data '{"text":"Zainstalowane, poczekaj na koniec aktualizacji i restart."}' http://localhost:8122/text_to_speech &&
 
