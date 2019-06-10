e5fdd546e73406d04d1b12b6b60c6807a74ad3f3
araczkowski <araczkowski@gmail.com>
2019-06-10 10:02:38 +0200

    0.94 first beta;

---
+++ upgrades/0.94.sh
@@ -0,0 +1,39 @@
+#
+#   AIS dom upgrade script to version 0.94.x
+#
+#   Homepage: https://ai-speaker.com
+#
+# curl https://raw.githubusercontent.com/sviete/AIS-utils/master/upgrades/0.94.sh | bash
+################################################
+echo "-----------------------------"
+echo "-----------------------------"
+echo "-----------------------------"
+echo $(date '+%Y %b %d %H:%M') START
+echo "-----------------------------"
+echo "AIS dom upgrade to version 0.94.x"
+echo "-----------------------------"
+echo "-----------------------------"
+
+
+curl --header "Content-Type: application/json" --max-time 2 --request POST --data '{"text":"Start aktualizacji do wersji 0.94 To potrwa 20 minut. Poczekaj."}' http://localhost:8122/text_to_speech &&
+
+echo "-----------------------------";
+echo "Downloading lovelace json----";
+echo "-----------------------------";
+# curl -o "/data/data/pl.sviete.dom/files/home/AIS/.storage/lovelace" -L  https://raw.githubusercontent.com/sviete/AIS-utils/master/patches/scripts/lovelace_0.94 &&
+echo "-----------------------------";
+echo "-----------------------------";
+
+
+# remove the  spotify cache
+rm -rf /sdcard/Android/data/com.spotify.music/files/spotifycache
+
+# pip
+curl -o "/sdcard/ais-dom.tar.gz" -L https://raw.githubusercontent.com/sviete/AIS-utils/master/patches/scripts/ais-dom-0.94.0.tar.gz &&
+pip install /sdcard/ais-dom.tar.gz -U &&
+
+curl --header "Content-Type: application/json" --max-time 2 --request POST --data '{"text":"Zainstalowane, poczekaj na koniec aktualizacji i restart."}' http://localhost:8122/text_to_speech &&
+
+
+echo "all done"
+echo $(date '+%Y %b %d %H:%M') STOP
