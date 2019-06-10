031eea764143e92c904c2597caf3a68a76a32770
araczkowski <araczkowski@gmail.com>
2019-06-10 10:06:23 +0200

    0.94.1

--- upgrades/0.94.sh
+++ upgrades/0.94.sh
@@ -29,7 +29,7 @@ echo "-----------------------------";
 rm -rf /sdcard/Android/data/com.spotify.music/files/spotifycache
 
 # pip
-curl -o "/sdcard/ais-dom.tar.gz" -L https://raw.githubusercontent.com/sviete/AIS-utils/master/patches/scripts/ais-dom-0.94.0.tar.gz &&
+curl -o "/sdcard/ais-dom.tar.gz" -L https://raw.githubusercontent.com/sviete/AIS-utils/master/patches/scripts/ais-dom-0.94.1.tar.gz &&
 pip install /sdcard/ais-dom.tar.gz -U &&
 
 curl --header "Content-Type: application/json" --max-time 2 --request POST --data '{"text":"Zainstalowane, poczekaj na koniec aktualizacji i restart."}' http://localhost:8122/text_to_speech &&
