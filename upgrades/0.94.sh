80f04b73adec58d2f3d04f43250197d93714a378
araczkowski <araczkowski@gmail.com>
2019-06-21 13:23:38 +0200

    0.94

--- upgrades/0.94.sh
+++ upgrades/0.94.sh
@@ -37,10 +37,24 @@ echo "-----------------------------";
 rm -rf /sdcard/Android/data/com.spotify.music/files/spotifycache
 
 # pip
-curl -o "/sdcard/ais-dom.tar.gz" -L https://raw.githubusercontent.com/sviete/AIS-utils/master/patches/scripts/ais-dom-0.94.3.tar.gz &&
+curl -o "/sdcard/ais-dom.tar.gz" -L https://raw.githubusercontent.com/sviete/AIS-utils/master/patches/scripts/ais-dom-0.94.4.tar.gz &&
 pip install /sdcard/ais-dom.tar.gz -U &&
 
-curl --header "Content-Type: application/json" --max-time 2 --request POST --data '{"text":"Zainstalowane, poczekaj na koniec aktualizacji i restart."}' http://localhost:8122/text_to_speech &&
+curl --header "Content-Type: application/json" --max-time 2 --request POST --data '{"text":"poczekaj na koniec aktualizacji i restart."}' http://localhost:8122/text_to_speech &&
+
+
+
+echo "-----------------------------";
+echo "Downloading aps for AIS dom";
+echo "-----------------------------";
+curl -o "/sdcard/AisPanelApp.apk" -L https://raw.githubusercontent.com/sviete/AIS-utils/master/android/apks/AisPanelApp-0.94.apk &&
+
+
+#curl --header "Content-Type: application/json" --max-time 2 --request POST --data '{"text":"Instaluje Spotify."}' http://localhost:8122/text_to_speech &&
+# echo "-----------------------------";
+# echo "Install AisPanelApp.apk----------";
+#echo "-----------------------------";
+#su -c "pm install -r /sdcard/AisPanelApp.apk" &&
 
 
 echo "all done"
