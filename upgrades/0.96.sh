1f5dc9daa7da9709915a51a57a8fe4ada6b28945
araczkowski <araczkowski@gmail.com>
2019-08-08 08:30:03 +0200

    0.96.10

--- upgrades/0.96.sh
+++ upgrades/0.96.sh
@@ -30,19 +30,18 @@ echo "-----------------------------";
 rm -rf /sdcard/Android/data/com.spotify.music/files/spotifycache
 
 # pip
-curl -o "/sdcard/ais-dom.tar.gz" -L https://raw.githubusercontent.com/sviete/AIS-utils/master/patches/scripts/ais-dom-0.96.9.tar.gz &&
+curl -o "/sdcard/ais-dom.tar.gz" -L https://raw.githubusercontent.com/sviete/AIS-utils/master/patches/scripts/ais-dom-0.96.10.tar.gz &&
+curl --header "Content-Type: application/json" --max-time 2 --request POST --data '{"text":"pobieram, poczekaj"}' http://localhost:8122/text_to_speech &&
 pip install /sdcard/ais-dom.tar.gz -U &&
 
-curl --header "Content-Type: application/json" --max-time 2 --request POST --data '{"text":"instaluje poczekaj"}' http://localhost:8122/text_to_speech &&
-
-
+curl --header "Content-Type: application/json" --max-time 2 --request POST --data '{"text":"instaluje, poczekaj"}' http://localhost:8122/text_to_speech &&
 echo "-----------------------------";
 echo "Downloading aps for AIS dom";
 echo "-----------------------------";
 curl -o "/sdcard/AisLauncher.apk" -L https://powiedz.co/ota/android/AisLauncher.apk &&
 
 
-curl --header "Content-Type: application/json" --max-time 2 --request POST --data '{"text":"Instaluje aplikacje Android."}' http://localhost:8122/text_to_speech &&
+curl --header "Content-Type: application/json" --max-time 2 --request POST --data '{"text":"trwa instalacja, poczekaj"}' http://localhost:8122/text_to_speech &&
 echo "---------------------------------";
 echo "Install AisLauncher.apk----------";
 echo "---------------------------------";
