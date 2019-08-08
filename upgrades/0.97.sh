34b7740a79c08147930bb8687ac4ca609c070ec9
araczkowski <araczkowski@gmail.com>
2019-08-08 12:34:39 +0200

    0.97.0

---
+++ upgrades/0.97.sh
@@ -0,0 +1,33 @@
+#
+#   AIS dom upgrade script to version 0.94.x
+#
+#   Homepage: https://ai-speaker.com
+#
+# curl https://raw.githubusercontent.com/sviete/AIS-utils/master/upgrades/0.97.sh | bash
+################################################
+echo "-----------------------------"
+echo "-----------------------------"
+echo "-----------------------------"
+echo $(date '+%Y %b %d %H:%M') START
+echo "-----------------------------"
+echo "AIS dom upgrade to version 0.97.x"
+echo "-----------------------------"
+echo "-----------------------------"
+
+
+curl --header "Content-Type: application/json" --max-time 2 --request POST --data '{"text":"Start aktualizacji. To potrwa 20 minut. Poczekaj."}' http://localhost:8122/text_to_speech &&
+
+
+
+# remove the  spotify cache
+rm -rf /sdcard/Android/data/com.spotify.music/files/spotifycache
+
+# pip
+curl -o "/sdcard/ais-dom.tar.gz" -L https://raw.githubusercontent.com/sviete/AIS-utils/master/patches/scripts/ais-dom-0.97.0.tar.gz &&
+pip install /sdcard/ais-dom.tar.gz -U &&
+
+curl --header "Content-Type: application/json" --max-time 2 --request POST --data '{"text":"poczekaj na koniec aktualizacji i restart."}' http://localhost:8122/text_to_speech &&
+
+
+echo "all done"
+echo $(date '+%Y %b %d %H:%M') STOP
