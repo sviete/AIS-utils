2a68a5b3de64b5db7047d251f08484bdefb89b5b
tomek75 <tomek@sviete.pl>
2019-10-12 14:21:10 +0200

    0.99.6

--- upgrades/0.99.sh
+++ upgrades/0.99.sh
@@ -24,7 +24,7 @@ rm -rf /sdcard/Android/data/com.spotify.music/files/spotifycache
 
 
 # pip
-curl -o "/sdcard/ais-dom.tar.gz" -L https://raw.githubusercontent.com/sviete/AIS-utils/master/patches/scripts/ais-dom-0.99.4.tar.gz &&
+curl -o "/sdcard/ais-dom.tar.gz" -L https://raw.githubusercontent.com/sviete/AIS-utils/master/patches/scripts/ais-dom-0.99.6.tar.gz &&
 pip install /sdcard/ais-dom.tar.gz -U &&
 
 
