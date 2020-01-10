0b4441f14bb8020f0f86373b7d86766a8af3840d
tomek75 <tomek@sviete.pl>
2020-01-10 10:46:04 +0100

    fix in add zigbee script

--- patches/add_zigbee.sh
+++ patches/add_zigbee.sh
@@ -12,20 +12,25 @@
 set -e
 
 # bash
+echo "1. replace bash profile"
 curl -o "/data/data/pl.sviete.dom/files/home/.bash_profile" -L  https://raw.githubusercontent.com/sviete/AIS-utils/master/patches/scripts/.bash_profile
 chmod +x /data/data/pl.sviete.dom/files/home/.bash_profile
 
 # install python2
-apt install git python2 -y
+echo "2. install python2"
+apt install python2 -y
 
 # download zigbee
+echo "3. download zigbee.zip"
 curl -o /data/data/pl.sviete.dom/files/home/zigbee.zip -L  https://raw.githubusercontent.com/sviete/AIS-utils/master/patches/scripts/zigbee.zip
 
 # unzip zigbee
-7z x -mmt=2 -o /data/data/pl.sviete.dom/files/home/zigbee2mqtt
+echo "4. unzip zigbee2mqtt"
+7z x -mmt=2 -o/data/data/pl.sviete.dom/files/home/zigbee2mqtt /data/data/pl.sviete.dom/files/home/zigbee.zip -y
 
 # remove zip
-rm /data/data/pl.sviete.dom/files/home/zigbee.zip /data/data/pl.sviete.dom/files/home/zigbee.zip
+echo "5. remove zigbee.zip"
+rm /data/data/pl.sviete.dom/files/home/zigbee.zip
 
 echo "-----------------------------"
 echo $(date '+%Y %b %d %H:%M')
