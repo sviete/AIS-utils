cf618b11566ae07710e643d84a3f88fc1460b977
tomek75 <tomek@sviete.pl>
2020-09-11 15:23:58 +0200

    old keylayout for gates before 2.0 ais dom server

---
+++ linux/old_ais_keylayout.sh
@@ -0,0 +1,16 @@
+#!/data/data/pl.sviete.dom/files/usr/bin/bash
+#
+#   AIS
+#
+#   Homepage: https://ai-speaker.com
+#
+################################################
+#  Change generic key layout
+#  run it by executiong in AIS dom console:
+# curl https://raw.githubusercontent.com/sviete/AIS-utils/master/linux/old_ais_keylayout.sh | bash
+#
+echo "AIS Download OLD Generic.kl START"
+curl -o "/sdcard/Generic.kl" -L https://raw.githubusercontent.com/sviete/AIS-utils/master/patches/OLD_Generic.kl
+echo " "
+su -c "mount -o rw,remount,rw /system && cp /sdcard/Generic.kl /system/usr/keylayout/Generic.kl && rm /sdcard/Generic.kl && mount -o ro,remount,ro /system && echo done"
+echo "AIS Generic.kl ALL DONE"
