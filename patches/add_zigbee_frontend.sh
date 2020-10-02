3d174fd990e0d6258000fa856c132eb7a334d3f6
tomek75 <tomek@sviete.pl>
2020-10-02 11:46:31 +0200

    fix

--- patches/add_zigbee_frontend.sh
+++ patches/add_zigbee_frontend.sh
@@ -17,21 +17,15 @@ echo "----ADD ZIGBEE FRONTEND START ----"
 echo "-----------------------------"
 
 # bash
-string1="frontend"
-string2="experimental"
-file="/data/data/pl.sviete.dom/files/home/zigbee2mqtt/data/configuration.yaml"
-if grep -q "$string" "$file" ; then
+if ! grep -qF "frontend" /data/data/pl.sviete.dom/files/home/zigbee2mqtt/data/configuration.yaml ; then
   echo "Configuration without frontend - adding"
-  echo "" >> $file
-  echo "frontend:" >> $file
-  echo "  port: 8099" >> $file
+  echo "" >> /data/data/pl.sviete.dom/files/home/zigbee2mqtt/data/configuration.yaml
+  echo "frontend:" >> /data/data/pl.sviete.dom/files/home/zigbee2mqtt/data/configuration.yaml
+  echo "  port: 8099" >> /data/data/pl.sviete.dom/files/home/zigbee2mqtt/data/configuration.yaml
+  echo "experimental:" >> /data/data/pl.sviete.dom/files/home/zigbee2mqtt/data/configuration.yaml
+  echo "  new_api: true" >> /data/data/pl.sviete.dom/files/home/zigbee2mqtt/data/configuration.yaml
 fi
-if grep -q "$string2" "$file" ; then
-  echo "Configuration without new_api - adding"
-  echo "" >> $file
-  echo "experimental:" >> $file
-  echo "  new_api: true" >> $file
-fi
+
 echo "-----------------------------"
 echo $(date '+%Y %b %d %H:%M')
 echo "----ALL OK END----"
