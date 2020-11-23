e174b3b856a7c7ef08b17156006c590a76f54c14
tomek75 <tomek@sviete.pl>
2020-11-23 15:12:00 +0100

    fix curl get

--- linux/DEV2/apt_upgrade.sh
+++ linux/DEV2/apt_upgrade.sh
@@ -5,7 +5,7 @@
 #  Upgrade AIS dom binary repo
 # curl https://raw.githubusercontent.com/sviete/AIS-utils/master/linux/DEV2/apt_upgrade.sh | bash
 #
-curl GET http://localhost:8122/text_to_speech?text=Zmiana repozytorium AIS dom
+curl http://localhost:8122/text_to_speech?text=Zmiana%20repozytorium%20AIS%20dom
 echo " "
 echo "Upgrade repo"
 echo "# The main AI-Speaker repository:" > /data/data/pl.sviete.dom/files/usr/etc/apt/sources.list
@@ -15,7 +15,7 @@ echo " "
 apt update
 apt install libacl
 echo " "
-curl GET http://localhost:8122/text_to_speech?text=Start instalacji binarnych pakietów
+curl http://localhost:8122/text_to_speech?text=Start%20instalacji%20binarnych%20pakiet%C3%B3w
 echo "APT install"
 apt install -y apt
 apt install -y clang
@@ -23,25 +23,25 @@ apt install -y ldc
 apt upgrade -y
 apt install -y libusb
 echo "Fix FTP serwer"
-curl GET http://localhost:8122/text_to_speech?text=Zmiana serwera FTP
+curl http://localhost:8122/text_to_speech?text=Zmiana%20serwera%20FTP
 pm2 delete ftp
 pm2 start busybox --name ftp --output  /dev/null --error  /dev/null --restart-delay=150000 -- tcpsvd -vE 0.0.0.0 1024 busybox ftpd -w /sdcard
 pm2 save
 echo "Python upgrade"
-curl GET http://localhost:8122/text_to_speech?text=Aktualizacja python do wersji 3.9
+curl http://localhost:8122/text_to_speech?text=Aktualizacja%20python
 apt remove python
 rm -rf /data/data/pl.sviete.dom/files/usr/lib/python3.7
 apt install python
 echo "Nodejs uprgrade"
-curl GET http://localhost:8122/text_to_speech?text=Zmiana wersji nodejs na LTS
+curl http://localhost:8122/text_to_speech?text=Aktualizacja%20nodejs
 apt remove nodejs
 apt install nodejs-lts
 echo "ais-dom install"
-curl GET http://localhost:8122/text_to_speech?text=Instalacja Asystenta domowego
+curl http://localhost:8122/text_to_speech?text=Instalacja%20Asystenta%20domowego
 pip install --upgrade pip
 pip install ais-dom
 pip install bcrypt==3.1.7
 pip install sqlalchemy==1.3.19
 echo "PM2 restart AIS"
-curl GET http://localhost:8122/text_to_speech?text=Ponowne uruchomienie usługi Asystent domowy
+curl http://localhost:8122/text_to_speech?text=Ponowne%20uruchomienie%20us%C5%82ugi%20Asystent%20domowy
 pm2 restart ais
