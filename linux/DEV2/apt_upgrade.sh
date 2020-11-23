580c5e78eeb1df6348cb2d7326a7e87b573a5a43
tomek75 <tomek@sviete.pl>
2020-11-23 15:06:18 +0100

    fix

--- linux/DEV2/apt_upgrade.sh
+++ linux/DEV2/apt_upgrade.sh
@@ -15,32 +15,33 @@ echo " "
 apt update
 apt install libacl
 echo " "
-curl GET http://localhost:8122/text_to_speech?text=Start instalacji składowych
-echo " "
+curl GET http://localhost:8122/text_to_speech?text=Start instalacji binarnych pakietów
+echo "APT install"
+apt install -y apt
 apt install -y clang
 apt install -y ldc
-apt install apt
 apt upgrade -y
-apt install libusb
-echo " "
+apt install -y libusb
+echo "Fix FTP serwer"
 curl GET http://localhost:8122/text_to_speech?text=Zmiana serwera FTP
 pm2 delete ftp
 pm2 start busybox --name ftp --output  /dev/null --error  /dev/null --restart-delay=150000 -- tcpsvd -vE 0.0.0.0 1024 busybox ftpd -w /sdcard
 pm2 save
-echo " "
-curl GET http://localhost:8122/text_to_speech?text=Zmiana wersji python na 3.9
+echo "Python upgrade"
+curl GET http://localhost:8122/text_to_speech?text=Aktualizacja python do wersji 3.9
 apt remove python
 rm -rf /data/data/pl.sviete.dom/files/usr/lib/python3.7
 apt install python
-echo " "
+echo "Nodejs uprgrade"
 curl GET http://localhost:8122/text_to_speech?text=Zmiana wersji nodejs na LTS
 apt remove nodejs
 apt install nodejs-lts
-echo " "
+echo "ais-dom install"
 curl GET http://localhost:8122/text_to_speech?text=Instalacja Asystenta domowego
 pip install --upgrade pip
 pip install ais-dom
 pip install bcrypt==3.1.7
 pip install sqlalchemy==1.3.19
-echo " "
+echo "PM2 restart AIS"
 curl GET http://localhost:8122/text_to_speech?text=Ponowne uruchomienie usługi Asystent domowy
+pm2 restart ais
