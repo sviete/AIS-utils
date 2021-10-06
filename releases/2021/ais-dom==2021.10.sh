01aafa72d98a4cb9b355cd7a96e3729788a8e2a6
tomek75 <tomek@sviete.pl>
2021-10-06 15:42:01 +0200

    SECONDS

--- releases/2021/ais-dom==2021.10.sh
+++ releases/2021/ais-dom==2021.10.sh
@@ -1,15 +1,30 @@
 #!/data/data/pl.sviete.dom/files/usr/bin/bash
-#   AIS
-#   Homepage: https://ai-speaker.com
+# AIS
+# omepage: https://ai-speaker.com
 ################################################
-#  Fix key layout
-#  run it by executiong in AIS dom console:
-# curl https://raw.githubusercontent.com/sviete/AIS-utils/master/releases/2021/ais-dom==2021.10.sh | bash
+# Install ais-dom 2021.10...
+# run it by executiong in AIS dom console:
+# curl -L https://raw.githubusercontent.com/sviete/AIS-utils/master/releases/2021/ais-dom==2021.10.sh | bash
 #
+SECONDS=0
 
-echo "AIS 2021.10 installation START"
+echo -e '\e[40;38;5;82m START instalacji wersji \e[30;48;5;82m AIS 2021.10 \e[0m'
+curl http://localhost:8122/text_to_speech?text=Start%20instalacji%20instalacji%20wersji%202021.10
+echo -e '\e[40;38;5;82m START instalacji wersji 2021.10 \e[30;48;5;82m AIS \e[0m'
+
+echo -e '\e[38;5;11m Pobieram ... \e[0m'
+curl http://localhost:8122/text_to_speech?text=Pobieram
 curl -o "/data/data/pl.sviete.dom/files/home/AIS/2021.10_wheelhouse.tar.7z" -L  https://github.com/sviete/AIS-utils/blob/master/releases/2021/2021.10_wheelhouse.tar.7z?raw=true
+
+echo -e '\e[38;5;214m Rozpakowuje ... \e[0m'
+curl http://localhost:8122/text_to_speech?text=Rozpakowuje
 7z x -mmt=2 -o/data/data/pl.sviete.dom/files/home/AIS/ /data/data/pl.sviete.dom/files/home/AIS/2021.10_wheelhouse.tar.7z -y
+
+echo -e '\e[38;5;202m Instaluje ... \e[0m'
+curl http://localhost:8122/text_to_speech?text=Instaluje
 cd /data/data/pl.sviete.dom/files/home/AIS
 pip install -r wheels/requirements.txt --no-index --find-links wheels/
-echo "All done OK. AIS"
+
+echo -e '\e[38;5;27m Restartuje usługę ais ... \e[0m'
+curl http://localhost:8122/text_to_speech?text=Restartuje%20us%C5%82ug%C4%99%20ais
+echo -e '\e[40;38;5;82m Wszystko OK. Instalacja trwała \e[30;48;5;82m $SECONDS sekund \e[0m'
