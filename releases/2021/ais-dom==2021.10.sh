e65d40bb76f08d2c7042a0ed82ee4a53924934b7
tomek75 <tomek@sviete.pl>
2021-10-06 16:56:24 +0200

    rm wheelhouse

--- releases/2021/ais-dom==2021.10.sh
+++ releases/2021/ais-dom==2021.10.sh
@@ -25,6 +25,9 @@ echo -e '\e[38;5;202m Instaluje AIS ... \e[0m'
 curl http://localhost:8122/text_to_speech?text=Instaluje%20AIS
 cd /data/data/pl.sviete.dom/files/home/AIS
 pip install -r wheels/requirements.txt --no-index --find-links wheels/
+rm -rf /data/data/pl.sviete.dom/files/home/AIS/wheels
+rm -rf /data/data/pl.sviete.dom/files/home/AIS/wheels/data/data/pl.sviete.dom/files/home/AIS/2021.10_wheelhouse.tar.7z
+
 
 
 echo -e '\e[38;5;11m Pobieram Zigbee ... \e[0m'
