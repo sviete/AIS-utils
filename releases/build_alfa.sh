6c2c1b55379d0586d0996f4c57b8e37ccdb32f89
tomek75 <tomek@sviete.pl>
2021-10-08 10:29:01 +0200

    pre_alfa

---
+++ releases/build_alfa.sh
@@ -0,0 +1,33 @@
+#!/data/data/pl.sviete.dom/files/usr/bin/bash
+# AIS
+# omepage: https://ai-speaker.com
+################################################
+# Install ais-dom on ALPHA chanel
+# run it by executiong in AIS dom console:
+# curl -L https://raw.githubusercontent.com/sviete/AIS-utils/master/releases/build_alfa.sh | bash
+#
+
+
+echo -e '\e[38;5;220m START bulding version \e[30;48;5;208m AIS ALFA \e[0m'
+SECONDS=0
+echo -e '\e[38;5;220m Download AIS codes ... \e[0m'
+git checkout beta
+git pull
+
+echo -e '\e[38;5;220m Building AIS wheels ... \e[0m'
+export CRYPTOGRAPHY_DONT_BUILD_RUST=1
+pip wheel . -w wheels
+pip wheel python-miio==0.5.8 -w wheels
+cp requirements.txt wheels
+cd wheels
+rename cp39-cp39-linux_armv7l py3-none-any *
+sed -i '1d' requirements.txt
+echo \ >> requirements.txt
+echo ais-dom==2021.10.1b0 >> requirements.txt
+
+echo -e '\e[38;5;220m Packing AIS ... \e[0m'
+cd ..
+7za a -m0=lzma2 alfa.tar.7z wheels
+
+
+echo -e '\e[40;38;5;220m All OK. The packaing took \e[30;48;5;208m' $SECONDS 'seconds \e[0m'
---
+++ releases/pre_alfa.sh
@@ -0,0 +1,55 @@
+#!/data/data/pl.sviete.dom/files/usr/bin/bash
+# AIS
+# omepage: https://ai-speaker.com
+################################################
+# Install ais-dom on ALPHA chanel
+# run it by executiong in AIS dom console:
+# curl -L https://raw.githubusercontent.com/sviete/AIS-utils/master/releases/pre_alfa.sh | bash
+#
+
+
+echo -e '\e[38;5;220m START instalacji wersji \e[30;48;5;208m AIS PRE ALFA \e[0m'
+curl http://localhost:8122/text_to_speech?text=Start%20instalacji%20wersji%202021.10
+sleep 5
+
+SECONDS=0
+echo -e '\e[38;5;220m Pobieram AIS ... \e[0m'
+curl http://localhost:8122/text_to_speech?text=Pobieram%20AIS
+curl -H 'Cache-Control: no-cache' -o "/data/data/pl.sviete.dom/files/home/AIS/pre_alfa.tar.7z" -L  https://github.com/sviete/AIS-utils/blob/master/releases/2021/2021.10_wheelhouse.tar.7z?raw=true
+
+echo -e '\e[38;5;220m Rozpakowuje AIS ... \e[0m'
+curl http://localhost:8122/text_to_speech?text=Rozpakowuje%20AIS
+7z x -mmt=2 -o/data/data/pl.sviete.dom/files/home/AIS/ /data/data/pl.sviete.dom/files/home/AIS/2021.10_wheelhouse.tar.7z -y
+
+echo -e '\e[38;5;220m Instaluje AIS ... \e[0m'
+curl http://localhost:8122/text_to_speech?text=Instaluje%20AIS
+cd /data/data/pl.sviete.dom/files/home/AIS
+pip install -r wheels/requirements.txt --no-index --find-links wheels -U
+rm -rf /data/data/pl.sviete.dom/files/home/AIS/wheels
+rm -rf /data/data/pl.sviete.dom/files/home/AIS/pre_alfa.tar.7z
+
+
+
+echo -e '\e[38;5;220m Pobieram Zigbee ... \e[0m'
+curl http://localhost:8122/text_to_speech?text=Pobieram%20Zigbee
+curl -H 'Cache-Control: no-cache' -o "/data/data/pl.sviete.dom/files/home/AIS/zigbee_update.zip" -L http://powiedz.co/ota/zigbee_beta.zip
+cp -R /data/data/pl.sviete.dom/files/home/zigbee2mqtt/data /data/data/pl.sviete.dom/files/home/data-backup
+rm -rf /data/data/pl.sviete.dom/files/home/zigbee2mqtt
+
+echo -e '\e[38;5;220m Rozpakowuje Zigbee ... \e[0m'
+curl http://localhost:8122/text_to_speech?text=Rozpakowuje%20Zigbee
+7z x -mmt=2 -o/data/data/pl.sviete.dom/files/home/zigbee2mqtt /data/data/pl.sviete.dom/files/home/AIS/zigbee_update.zip -y
+rm -rf /data/data/pl.sviete.dom/files/home/AIS/zigbee_update.zip
+cp -R /data/data/pl.sviete.dom/files/home/data-backup/* /data/data/pl.sviete.dom/files/home/zigbee2mqtt/data
+rm -rf /data/data/pl.sviete.dom/files/home/data-backup
+
+
+echo -e '\e[40;38;5;220m Wszystko OK. Instalacja trwała \e[30;48;5;208m' $SECONDS 'sekund \e[0m'
+curl http://localhost:8122/text_to_speech?text=Instalacja%20trwa%C5%82a%20$SECONDS%20sekund.%20Poczekaj%20na%20ponowne%20uruchomienie%20systemu.
+
+echo "21.10.01" > /data/data/pl.sviete.dom/files/home/AIS/.ais_apt
+
+sleep 6
+echo -e '\e[38;5;220m Restartuje usługę ais ... \e[0m'
+curl http://localhost:8122/text_to_speech?text=Restartuje%20us%C5%82ug%C4%99%20AIS
+pm2 restart ais
