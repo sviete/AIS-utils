9d101269e19d3a2a94d407464ad32049c0edc223
tomek75 <tomek@sviete.pl>
2021-10-11 10:49:30 +0200

    pre alfa

--- releases/pre_alfa.sh
+++ releases/pre_alfa.sh
@@ -7,7 +7,6 @@
 # curl -L https://raw.githubusercontent.com/sviete/AIS-utils/master/releases/pre_alfa.sh | bash
 #
 
-
 echo -e '\e[38;5;220m START instalacji wersji \e[30;48;5;208m AIS PRE ALFA \e[0m'
 curl http://localhost:8122/text_to_speech?text=Start%20instalacji%20wersji%AIS%20ALFA
 
@@ -19,8 +18,10 @@ apt install -y neofetch
 neofetch --source  ~/AIS/logo.txt
 
 SECONDS=0
+
 echo -e '\e[38;5;220m Pobieram AIS ... \e[0m'
 curl http://localhost:8122/text_to_speech?text=Pobieram%20AIS
+curl -X POST http://localhost:8180/api/webhook/aisdomprocesscommandfromframe -H 'Content-Type: application/json' -d '{"topic":"ais/set_update_status", "payload": "downloading"}'
 curl -o "/data/data/pl.sviete.dom/files/home/AIS/pre_alfa_wheelhouse.tar.7z" -L  https://github.com/sviete/AIS-utils/blob/master/releases/pre_alfa_wheelhouse.tar.7z?raw=true
 
 echo -e '\e[38;5;220m Rozpakowuje AIS ... \e[0m'
@@ -29,6 +30,7 @@ curl http://localhost:8122/text_to_speech?text=Rozpakowuje%20AIS
 
 echo -e '\e[38;5;220m Instaluje AIS ... \e[0m'
 curl http://localhost:8122/text_to_speech?text=Instaluje%20AIS
+curl -X POST http://localhost:8180/api/webhook/aisdomprocesscommandfromframe -H 'Content-Type: application/json' -d '{"topic":"ais/set_update_status", "payload": "installing"}'
 cd /data/data/pl.sviete.dom/files/home/AIS
 pip install -r wheels/requirements.txt --no-index --find-links wheels -U
 rm -rf /data/data/pl.sviete.dom/files/home/AIS/wheels
@@ -52,6 +54,9 @@ else
   rm -rf /data/data/pl.sviete.dom/files/home/data-backup
 fi
 
+# android app
+# su -c 'am start -n launcher.sviete.pl.domlauncherapp/.LauncherActivity -e command ais-dom-update
+# su -c 'am start -n launcher.sviete.pl.domlauncherapp/.LauncherActivity -e command ais-dom-update-beta
 
 
 echo -e '\e[40;38;5;220m Wszystko OK. Instalacja trwała \e[30;48;5;208m' $SECONDS 'sekund \e[0m'
@@ -62,4 +67,5 @@ echo "21.10.01" > /data/data/pl.sviete.dom/files/home/AIS/.ais_apt
 sleep 6
 echo -e '\e[38;5;220m Restartuje usługę ais ... \e[0m'
 curl http://localhost:8122/text_to_speech?text=Restartuje%20us%C5%82ug%C4%99%20AIS
+curl -X POST http://localhost:8180/api/webhook/aisdomprocesscommandfromframe -H 'Content-Type: application/json' -d '{"topic":"ais/set_update_status", "payload": "restart"}'
 pm2 restart ais
