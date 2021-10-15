7e6fb50ba2d94f3d985b0f7e87dadfaa3113a507
tomek75 <tomek@sviete.pl>
2021-10-15 14:00:05 +0200

    test version

--- releases/pre_alfa.sh
+++ releases/pre_alfa.sh
@@ -11,7 +11,7 @@
 AIS_VERSSION="21.10.06"
 AIS_ZIGBEE_VERSION='"version": "1.21.2",'
 AIS_ANDROID_VERSSION="versionName=3.0.0"
-AIS_VERSSION_PRE="210919"
+AIS_VERSSION_OLD="210919"
 
 echo -e '\e[38;5;220m START instalacji wersji \e[30;48;5;208m AIS PRE ALFA ' "$AIS_VERSSION" '\e[0m'
 curl http://localhost:8122/text_to_speech?text=Start%20instalacji%20wersji%AIS%20ALFA
@@ -23,22 +23,22 @@ apt install -y neofetch
 
 neofetch --source  ~/AIS/logo.txt
 
-# PRE requirements
+# TEST requirements
 
 AIS_CURR_VERSION=`cat /data/data/pl.sviete.dom/files/home/AIS/.ais_apt`
 echo -e '\e[38;5;220m Aktualizacja AIS z wersji ' "$AIS_CURR_VERSION" ' \e[0m'
 AIS_CURR_VERSION="${AIS_CURR_VERSION//.}"
 
-if [ $AIS_CURR_VERSION -ge $AIS_VERSSION_PRE ]
+if [ $AIS_CURR_VERSION -ge $AIS_VERSSION_OLD ]
   then
-    echo -e '\e[38;5;220m PRE VERSION TEST \e[30;48;5;208m OK \e[0m'
+    echo -e '\e[38;5;220m VERSION TEST \e[30;48;5;208m OK \e[0m'
   else
     curl http://localhost:8122/text_to_speech?text=Stop%20niezgodno%C5%9B%C4%87%20wersji
     curl -X POST http://localhost:8180/api/webhook/aisdomprocesscommandfromframe -H 'Content-Type: application/json' -d '{"topic":"ais/set_update_status", "payload": "outdated"}'
     echo -e '\e[38;5;220m Niezgodność wersji \e[30;48;5;208m EXIT\e[0m'
-    echo -e '\e[38;5;220m PRE VERSION NOK \e[30;48;5;208m STOP\e[0m'
-    echo -e '\e[38;5;220m PRE VERSION NOK \e[30;48;5;208m STOP\e[0m'
-    echo -e '\e[38;5;220m PRE VERSION NOK \e[30;48;5;208m STOP\e[0m'
+    echo -e '\e[38;5;220m VERSION NOK \e[30;48;5;208m STOP\e[0m'
+    echo -e '\e[38;5;220m VERSION NOK \e[30;48;5;208m STOP\e[0m'
+    echo -e '\e[38;5;220m VERSION NOK \e[30;48;5;208m STOP\e[0m'
 
     exit 1
 fi
