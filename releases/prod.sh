8022e77b3fab9adaa2e9e6bbb1071314cd1c1d7f
tomek75 <tomek@sviete.pl>
2021-10-15 15:27:25 +0200

    stop

--- releases/prod.sh
+++ releases/prod.sh
@@ -40,12 +40,12 @@ if [ $AIS_CURR_VERSION -ge $AIS_VERSSION_OLD ]
   else
     curl http://localhost:8122/text_to_speech?text=Stop%20niezgodno%C5%9B%C4%87%20wersji
     curl -X POST http://localhost:8180/api/webhook/aisdomprocesscommandfromframe -H 'Content-Type: application/json' -d '{"topic":"ais/set_update_status", "payload": "outdated"}'
-    echo -e '\e[38;5;220m Niezgodność wersji \e[30;48;5;208m EXIT\e[0m'
-    echo -e '\e[38;5;220m Aktualizacja możliwa z wersji co najmniej \e[30;48;5;208m' "$AIS_VERSSION_OLD" '\e[0m'
-    echo -e '\e[30;48;5;208m Wykonaj pełny reset aplikacji\e[0m'
-    echo -e '\e[38;5;220m VERSION NOK \e[30;48;5;208m STOP\e[0m'
-    echo -e '\e[38;5;220m VERSION NOK \e[30;48;5;208m STOP\e[0m'
-    echo -e '\e[38;5;220m VERSION NOK \e[30;48;5;208m STOP\e[0m'
+    echo -e '\e[38;5;220m Niezgodność wersji \e[30;48;5;208m EXIT \e[0m'
+    echo -e '\e[38;5;220m Aktualizacja tym skryptem jest możliwa z wersji co najmniej \e[30;48;5;208m' "$AIS_VERSSION_OLD" '\e[0m'
+    echo -e '\e[30;48;5;208m Wykonaj pełny reset aplikacji \e[0m'
+    echo -e '\e[38;5;220m VERSION NOK \e[30;48;5;208m STOP \e[0m'
+    echo -e '\e[38;5;220m VERSION NOK \e[30;48;5;208m STOP \e[0m'
+    echo -e '\e[38;5;220m VERSION NOK \e[30;48;5;208m STOP \e[0m'
 
     exit 1
 fi
