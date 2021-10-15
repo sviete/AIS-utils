1b3fe9f780e8ddaa65381529f3e0d7a720a09933
tomek75 <tomek@sviete.pl>
2021-10-15 13:44:30 +0200

    exit 1

--- releases/pre_alfa.sh
+++ releases/pre_alfa.sh
@@ -11,8 +11,9 @@
 AIS_VERSSION="21.10.06"
 AIS_ZIGBEE_VERSION='"version": "1.21.2",'
 AIS_ANDROID_VERSSION="versionName=3.0.0"
+AIS_VERSSION_PRE="210919"
 
-echo -e '\e[38;5;220m START instalacji wersji \e[30;48;5;208m AIS PRE ALFA ' $AIS_VERSSION '\e[0m'
+echo -e '\e[38;5;220m START instalacji wersji \e[30;48;5;208m AIS PRE ALFA ' "$AIS_VERSSION" '\e[0m'
 curl http://localhost:8122/text_to_speech?text=Start%20instalacji%20wersji%AIS%20ALFA
 
 apt update
@@ -22,10 +23,30 @@ apt install -y neofetch
 
 neofetch --source  ~/AIS/logo.txt
 
+# PRE requirements
+
+AIS_CURR_VERSION=`cat /data/data/pl.sviete.dom/files/home/AIS/.ais_apt`
+echo -e '\e[38;5;220m Aktualizacja AIS z wersji ' "$AIS_CURR_VERSION" ' \e[0m'
+AIS_CURR_VERSION="${AIS_CURR_VERSION//.}"
+
+if [ $AIS_CURR_VERSION -ge $AIS_VERSSION_PRE ]
+  then
+    echo -e '\e[38;5;220m PRE VERSION \e[30;48;5;208m OK\e[0m'
+  else
+    curl http://localhost:8122/text_to_speech?text=Stop%20niezgodno%C5%9B%C4%87%20wersji
+    curl -X POST http://localhost:8180/api/webhook/aisdomprocesscommandfromframe -H 'Content-Type: application/json' -d '{"topic":"ais/set_update_status", "payload": "outdated"}'
+    echo -e '\e[38;5;220m PRE VERSION NOK \e[30;48;5;208m STOP\e[0m'
+    echo -e '\e[38;5;220m PRE VERSION NOK \e[30;48;5;208m STOP\e[0m'
+    echo -e '\e[38;5;220m PRE VERSION NOK \e[30;48;5;208m STOP\e[0m'
+
+    exit 1
+fi
+
+
+
 SECONDS=0
 
 # AIS PYTHON APP
-
 echo -e '\e[38;5;220m Pobieram AIS ... \e[0m'
 curl http://localhost:8122/text_to_speech?text=Pobieram%20AIS
 curl -X POST http://localhost:8180/api/webhook/aisdomprocesscommandfromframe -H 'Content-Type: application/json' -d '{"topic":"ais/set_update_status", "payload": "downloading"}'
@@ -45,8 +66,8 @@ rm -rf /data/data/pl.sviete.dom/files/home/AIS/pre_alfa_wheelhouse.tar.7z
 
 
 # AIS ZIGBEE APP
-echo -e '\e[38;5;220m ZIGBEE \e[30;48;5;208m ' $AIS_ZIGBEE_VERSION '\e[0m'
-if  grep -q $AIS_ZIGBEE_VERSION '/data/data/pl.sviete.dom/files/home/zigbee2mqtt/package.json' ; then
+echo -e '\e[38;5;220m ZIGBEE \e[30;48;5;208m ' "$AIS_ZIGBEE_VERSION" '\e[0m'
+if  grep -q "$AIS_ZIGBEE_VERSION" '/data/data/pl.sviete.dom/files/home/zigbee2mqtt/package.json' ; then
   echo -e '\e[38;5;220m Zigbee OK... \e[0m'
 else
   echo -e '\e[38;5;220m Pobieram Zigbee ... \e[0m'
@@ -64,16 +85,16 @@ else
 fi
 
 
-echo -e '\e[40;38;5;220m OK. Instalacja trwała \e[30;48;5;208m' $SECONDS 'sekund \e[0m'
+echo -e '\e[40;38;5;220m OK. Instalacja trwała \e[30;48;5;208m' "$SECONDS" 'sekund \e[0m'
 curl http://localhost:8122/text_to_speech?text=Instalacja%20trwa%C5%82a%20$SECONDS%20sekund.%20Poczekaj%20na%20ponowne%20uruchomienie%20systemu.
 
-echo $AIS_VERSSION > /data/data/pl.sviete.dom/files/home/AIS/.ais_apt
+echo "$AIS_VERSSION" > /data/data/pl.sviete.dom/files/home/AIS/.ais_apt
 
 sleep 6
 
 # AIS ANDROID APP
-echo -e '\e[38;5;220m ANDROID \e[30;48;5;208m ' $AIS_ANDROID_VERSSION '\e[0m'
-if [ `su -c "dumpsys package pl.sviete.dom | grep versionName" | tr -d '[:space:]'` != $AIS_ANDROID_VERSSION ]; then
+echo -e '\e[38;5;220m ANDROID \e[30;48;5;208m ' "$AIS_ANDROID_VERSSION" '\e[0m'
+if [ `su -c "dumpsys package pl.sviete.dom | grep versionName" | tr -d '[:space:]'` != "$AIS_ANDROID_VERSSION" ]; then
     echo -e '\e[38;5;220m Pobieram i instaluje Android ... \e[0m'
     curl http://localhost:8122/text_to_speech?text=Pobieram%20Android%20i%20restartuje%20AIS
     curl -X POST http://localhost:8180/api/webhook/aisdomprocesscommandfromframe -H 'Content-Type: application/json' -d '{"topic":"ais/set_update_status", "payload": "restart"}'
@@ -85,3 +106,5 @@ else
     curl -X POST http://localhost:8180/api/webhook/aisdomprocesscommandfromframe -H 'Content-Type: application/json' -d '{"topic":"ais/set_update_status", "payload": "restart"}'
     pm2 restart ais
 fi
+
+exit 0
