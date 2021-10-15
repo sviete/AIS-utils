8d8ff617f74ad592ae7bd3e82a429112dd93ea1b
tomek75 <tomek@sviete.pl>
2021-10-15 13:55:25 +0200

    pre version test

--- releases/pre_alfa.sh
+++ releases/pre_alfa.sh
@@ -31,7 +31,7 @@ AIS_CURR_VERSION="${AIS_CURR_VERSION//.}"
 
 if [ $AIS_CURR_VERSION -ge $AIS_VERSSION_PRE ]
   then
-    echo -e '\e[38;5;220m PRE VERSION \e[30;48;5;208m OK\e[0m'
+    echo -e '\e[38;5;220m PRE VERSION TEST \e[30;48;5;208m OK \e[0m'
   else
     curl http://localhost:8122/text_to_speech?text=Stop%20niezgodno%C5%9B%C4%87%20wersji
     curl -X POST http://localhost:8180/api/webhook/aisdomprocesscommandfromframe -H 'Content-Type: application/json' -d '{"topic":"ais/set_update_status", "payload": "outdated"}'
