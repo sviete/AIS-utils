43dcaf23ad36d67c1d3e4158ed3e4c006b320e17
tomasz75 <tomek@sviete.pl>
2022-06-13 10:56:05 +0200

    2022.6.5

--- releases/python-3.10.sh
+++ releases/python-3.10.sh
@@ -3,8 +3,9 @@ apt update
 apt install libmd
 apt install openssl
 apt install libicu
+apt install openssl-tool
 apt -y upgrade
-npm install -g npm@8.7.0
+npm install -g npm@8.12.1
 cd ~/zigbee2mqtt/
 npm rebuild
 cd ~/AIS-webcmd/
@@ -16,6 +17,9 @@ apt install rust
 echo "[http]" > ~/.cargo/config
 echo 'cainfo = "/data/data/pl.sviete.dom/files/usr/etc/tls/cert.pem"' >> ~/.cargo/config
 export CARGO_BUILD_TARGET=armv7-linux-androideabi
+
+python3 -m pip install --upgrade pip
+MATHLIB="m" pip3 install numpy
 
 pip install cryptography
 pip install psycopg2
