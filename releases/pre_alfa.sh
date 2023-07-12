07dd72da3cf7175ccb3470ffdb3c8d46d8a67cc3
araczkowski <araczkowski@gmail.com>
2023-07-12 11:47:20 +0200

    Script version 2023.07.12.0

--- releases/pre_alfa.sh
+++ releases/pre_alfa.sh
@@ -8,6 +8,9 @@
 # chmod +x pre_alfa.sh
 # ./pre_alfa.sh
 
+echo -e '\e[38;5;220m Script version 2023.07.12.0 \e[0m'
+
+
 # AIS VERSIONS
 AIS_VERSSION="23.01.02"
 AIS_HA_VERSSION="2023.7.2"
@@ -48,6 +51,28 @@ fi
 
 SECONDS=0
 
+# AIS ZIGBEE APP
+echo -e '\e[38;5;220m ZIGBEE \e[30;48;5;208m ' "$AIS_ZIGBEE_VERSION" '\e[0m'
+if  grep -q "$AIS_ZIGBEE_VERSION" '/data/data/com.termux/files/home/zigbee2mqtt/package.json' ; then
+  echo -e '\e[38;5;220m Zigbee OK... \e[0m'
+else
+  echo -e '\e[38;5;220m Pobieram Zigbee ... \e[0m'
+  curl http://localhost:8122/text_to_speech?text=Pobieram%20Zigbee
+  curl -H 'Cache-Control: no-cache' -o "/data/data/com.termux/files/home/AIS/zigbee_update.zip" -L https://github.com/sviete/AIS-utils/blob/master/releases/zigbee_pre_alfa.zip?raw=true
+  curl -X POST http://localhost:8180/api/webhook/aisdomprocesscommandfromframe -H 'Content-Type: application/json' -d '{"topic":"ais/set_update_progress", "payload": "0.6:0.9"}'
+  cp -R /data/data/com.termux/files/home/zigbee2mqtt/data /data/data/com.termux/files/home/data-backup
+  rm -rf /data/data/com.termux/files/home/zigbee2mqtt
+
+  echo -e '\e[38;5;220m Rozpakowuje Zigbee ... \e[0m'
+  curl http://localhost:8122/text_to_speech?text=Rozpakowuje%20Zigbee
+  7z x -mmt=2 -o/data/data/com.termux/files/home/zigbee2mqtt /data/data/com.termux/files/home/AIS/zigbee_update.zip -y
+  rm -rf /data/data/com.termux/files/home/AIS/zigbee_update.zip
+  cp -R /data/data/com.termux/files/home/data-backup/* /data/data/com.termux/files/home/zigbee2mqtt/data
+  rm -rf /data/data/com.termux/files/home/data-backup
+fi
+curl -X POST http://localhost:8180/api/webhook/aisdomprocesscommandfromframe -H 'Content-Type: application/json' -d '{"topic":"ais/set_update_progress", "payload": "0.9:0.92"}'
+
+
 # AIS PYTHON APP
 echo -e '\e[38;5;220m Pobieram AIS ... \e[0m'
 curl http://localhost:8122/text_to_speech?text=Pobieram%20AIS
@@ -72,33 +97,19 @@ pip install -r wheels/requirements.txt --no-index --find-links=wheels -U
 curl -X POST http://localhost:8180/api/webhook/aisdomprocesscommandfromframe -H 'Content-Type: application/json' -d '{"topic":"ais/set_update_progress", "payload": "0.6:0.6"}'
 rm -rf /data/data/com.termux/files/home/AIS/wheels
 rm -rf /data/data/com.termux/files/home/AIS/pre_alfa_wheelhouse.tar.7z
-rm -rf /data/data/com.termux/files/usr/lib/python3.10
-cd ~/AIS-webcmd/
-npm install
-cd ~
-pm2 update
 
-
-# AIS ZIGBEE APP
-echo -e '\e[38;5;220m ZIGBEE \e[30;48;5;208m ' "$AIS_ZIGBEE_VERSION" '\e[0m'
-if  grep -q "$AIS_ZIGBEE_VERSION" '/data/data/com.termux/files/home/zigbee2mqtt/package.json' ; then
-  echo -e '\e[38;5;220m Zigbee OK... \e[0m'
-else
-  echo -e '\e[38;5;220m Pobieram Zigbee ... \e[0m'
-  curl http://localhost:8122/text_to_speech?text=Pobieram%20Zigbee
-  curl -H 'Cache-Control: no-cache' -o "/data/data/com.termux/files/home/AIS/zigbee_update.zip" -L https://github.com/sviete/AIS-utils/blob/master/releases/zigbee_pre_alfa.zip?raw=true
-  curl -X POST http://localhost:8180/api/webhook/aisdomprocesscommandfromframe -H 'Content-Type: application/json' -d '{"topic":"ais/set_update_progress", "payload": "0.6:0.9"}'
-  cp -R /data/data/com.termux/files/home/zigbee2mqtt/data /data/data/com.termux/files/home/data-backup
-  rm -rf /data/data/com.termux/files/home/zigbee2mqtt
-
-  echo -e '\e[38;5;220m Rozpakowuje Zigbee ... \e[0m'
-  curl http://localhost:8122/text_to_speech?text=Rozpakowuje%20Zigbee
-  7z x -mmt=2 -o/data/data/com.termux/files/home/zigbee2mqtt /data/data/com.termux/files/home/AIS/zigbee_update.zip -y
-  rm -rf /data/data/com.termux/files/home/AIS/zigbee_update.zip
-  cp -R /data/data/com.termux/files/home/data-backup/* /data/data/com.termux/files/home/zigbee2mqtt/data
-  rm -rf /data/data/com.termux/files/home/data-backup
-fi
-curl -X POST http://localhost:8180/api/webhook/aisdomprocesscommandfromframe -H 'Content-Type: application/json' -d '{"topic":"ais/set_update_progress", "payload": "0.9:0.92"}'
+# demonize
+apt -y install demonize
+echo 'cd ~/AIS-webcmd/' > /data/data/com.termux/files/home/AIS/ais_daemonize.sh
+echo 'npm install' > /data/data/com.termux/files/home/AIS/ais_daemonize.sh
+echo 'cd ~' > /data/data/com.termux/files/home/AIS/ais_daemonize.sh
+echo 'pm2 update' > /data/data/com.termux/files/home/AIS/ais_daemonize.sh
+echo 'pm2 restart ais' > /data/data/com.termux/files/home/AIS/ais_daemonize.sh
+echo 'pkill -9 python' > /data/data/com.termux/files/home/AIS/ais_daemonize.sh
+echo 'rm -rf /data/data/com.termux/files/usr/lib/python3.10' > /data/data/com.termux/files/home/AIS/ais_daemonize.sh
+echo 'rm -rf /data/data/com.termux/files/usr/lib/python3.11/site-packages/homeassistant/components/mqtt/device_tracker'
+chmod +x /data/data/com.termux/files/home/AIS/ais_daemonize.sh
+daemonize /data/data/com.termux/files/home/AIS/ais_daemonize.sh
 
 
 echo -e '\e[40;38;5;220m OK. Instalacja trwała \e[30;48;5;208m' "$SECONDS" 'sekund \e[0m'
@@ -109,24 +120,24 @@ echo "$AIS_VERSSION" > /data/data/com.termux/files/home/AIS/.ais_apt
 sleep 6
 
 
-# AIS ANDROID APP
-echo -e '\e[38;5;220m ANDROID \e[30;48;5;208m ' "$AIS_ANDROID_VERSSION" '\e[0m'
-if [ `su -c "dumpsys package com.termux | grep versionName" | tr -d '[:space:]'` != "$AIS_ANDROID_VERSSION" ]; then
-    echo -e '\e[38;5;220m Pobieram i instaluje Android ... \e[0m'
-    curl http://localhost:8122/text_to_speech?text=Pobieram%20Android%20i%20restartuje%20AIS
-    curl -X POST http://localhost:8180/api/webhook/aisdomprocesscommandfromframe -H 'Content-Type: application/json' -d '{"topic":"ais/set_update_progress", "payload": "0.95:0.95"}'
-    curl -X POST http://localhost:8180/api/webhook/aisdomprocesscommandfromframe -H 'Content-Type: application/json' -d '{"topic":"ais/set_update_status", "payload": "restart"}'
-    su -c 'am start -n launcher.sviete.pl.domlauncherapp/.LauncherActivity -e command ais-dom-update-beta'
-    curl -X POST http://localhost:8180/api/webhook/aisdomprocesscommandfromframe -H 'Content-Type: application/json' -d '{"topic":"ais/set_update_progress", "payload": "1:0.97"}'
-else
-    echo -e '\e[38;5;220m Android OK... \e[0m'
-    echo -e '\e[38;5;220m Restartuje usługę ais ... \e[0m'
-    curl http://localhost:8122/text_to_speech?text=Restartuje%20us%C5%82ug%C4%99%20AIS
-    curl -X POST http://localhost:8180/api/webhook/aisdomprocesscommandfromframe -H 'Content-Type: application/json' -d '{"topic":"ais/set_update_status", "payload": "restart"}'
-    curl -X POST http://localhost:8180/api/webhook/aisdomprocesscommandfromframe -H 'Content-Type: application/json' -d '{"topic":"ais/set_update_progress", "payload": "1:0.95"}'
-    # restart ais
-    pm2 restart ais
-    pkill -9 python
-fi
+# # AIS ANDROID APP
+# echo -e '\e[38;5;220m ANDROID \e[30;48;5;208m ' "$AIS_ANDROID_VERSSION" '\e[0m'
+# if [ `su -c "dumpsys package com.termux | grep versionName" | tr -d '[:space:]'` != "$AIS_ANDROID_VERSSION" ]; then
+#     echo -e '\e[38;5;220m Pobieram i instaluje Android ... \e[0m'
+#     curl http://localhost:8122/text_to_speech?text=Pobieram%20Android%20i%20restartuje%20AIS
+#     curl -X POST http://localhost:8180/api/webhook/aisdomprocesscommandfromframe -H 'Content-Type: application/json' -d '{"topic":"ais/set_update_progress", "payload": "0.95:0.95"}'
+#     curl -X POST http://localhost:8180/api/webhook/aisdomprocesscommandfromframe -H 'Content-Type: application/json' -d '{"topic":"ais/set_update_status", "payload": "restart"}'
+#     su -c 'am start -n launcher.sviete.pl.domlauncherapp/.LauncherActivity -e command ais-dom-update-beta'
+#     curl -X POST http://localhost:8180/api/webhook/aisdomprocesscommandfromframe -H 'Content-Type: application/json' -d '{"topic":"ais/set_update_progress", "payload": "1:0.97"}'
+# else
+#     echo -e '\e[38;5;220m Android OK... \e[0m'
+#     echo -e '\e[38;5;220m Restartuje usługę ais ... \e[0m'
+#     curl http://localhost:8122/text_to_speech?text=Restartuje%20us%C5%82ug%C4%99%20AIS
+#     curl -X POST http://localhost:8180/api/webhook/aisdomprocesscommandfromframe -H 'Content-Type: application/json' -d '{"topic":"ais/set_update_status", "payload": "restart"}'
+#     curl -X POST http://localhost:8180/api/webhook/aisdomprocesscommandfromframe -H 'Content-Type: application/json' -d '{"topic":"ais/set_update_progress", "payload": "1:0.95"}'
+#     # restart ais
+#     pm2 restart ais
+#     pkill -9 python
+# fi
 
 exit 0
