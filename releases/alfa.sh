2f6e3d4b07148fec40c2a5007d47fc0657eb93d8
Andrzej Raczkowski <araczkowski@gmail.com>
2023-08-08 00:23:43 +0200

    Update alfa.sh
    remove ./wheels/ha_av-10.1.1-py3-none-any.whl

--- releases/alfa.sh
+++ releases/alfa.sh
@@ -93,7 +93,6 @@ apt -y autoremove
 curl -X POST http://localhost:8180/api/webhook/aisdomprocesscommandfromframe -H 'Content-Type: application/json' -d '{"topic":"ais/set_update_status", "payload": "installing"}'
 cd /data/data/com.termux/files/home/AIS
 pip install -r wheels/requirements.txt --no-index --find-links=wheels -U
-pip install wheels/ha_av-10.1.1-py3-none-any.whl -U
 pip install pyinotify -U
 curl -X POST http://localhost:8180/api/webhook/aisdomprocesscommandfromframe -H 'Content-Type: application/json' -d '{"topic":"ais/set_update_progress", "payload": "0.6:0.6"}'
 rm -rf /data/data/com.termux/files/home/AIS/wheels
