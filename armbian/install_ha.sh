1647d0a0b0d49eaff9a4f2880955c6c1684f1e6f
asystentka-jolka <jolka@sviete.pl>
2023-08-18 18:39:34 +0200

    armbian

---
+++ armbian/install_ha.sh
@@ -0,0 +1,34 @@
+echo "add kernel boot parameters"
+sed -i '$ s/$/ systemd.unified_cgroup_hierarchy=0 apparmor=1 security=apparmor/' /boot/uEnv.txt
+
+echo "install apt packages"
+apt update
+apt upgrade -y
+apt install \
+apparmor \
+jq \
+wget \
+curl \
+udisks2 \
+libglib2.0-bin \
+network-manager \
+dbus \
+lsb-release \
+systemd-journal-remote \
+systemd-resolved -y
+
+echo "install docker"
+curl -fsSL get.docker.com | sh
+
+echo "install os-agent"
+curl -L https://github.com/home-assistant/os-agent/releases/download/1.5.1/os-agent_1.5.1_linux_aarch64.deb -o os-agent_1.5.1_linux_aarch64.deb
+dpkg -i ./os-agent_1.5.1_linux_aarch64.deb
+
+echo "install homeassistant-supervised"
+sed -i 's#PRETTY_NAME=.*#PRETTY_NAME="Debian GNU/Linux 11 (bullseye)"#'  /etc/os-release
+wget https://github.com/home-assistant/supervised-installer/releases/latest/download/homeassistant-supervised.deb
+apt install ./homeassistant-supervised.deb
+
+echo "enable services"
+sudo systemctl enable hassio-apparmor.service
+sudo systemctl enable hassio-supervisor.service
