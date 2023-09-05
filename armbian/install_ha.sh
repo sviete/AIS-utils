8a6099d3f8b9a278aa229b56f703678339759d1b
araczkowski <araczkowski@gmail.com>
2023-09-05 11:46:11 +0200

    prevent network connection from being overwritten

--- armbian/install_ha.sh
+++ armbian/install_ha.sh
@@ -29,6 +29,8 @@ udisks2 -y
 echo "fix DNS resolving"
 echo "nameserver 8.8.8.8" >> /etc/resolv.conf
 echo "nameserver 8.8.4.4" >> /etc/resolv.conf
+# prevent network connection from being overwritten
+# TODO
 
 echo "install docker"
 curl -fsSL get.docker.com | sh
